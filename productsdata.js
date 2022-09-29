177e016eeba8c9af904198e7207d2e79cbc7f587
Neha Patel <200501410@student.georgianc.on.ca>
2022-09-29 00:57:09 -0400

    Create productsdata.js

---
+++ productsdata.js
@@ -0,0 +1,23 @@
+
+var plugin = function(options) {
+    var seneca = this;
+
+    seneca.add({ role: 'product', cmd: 'add' }, function (msg, respond) {
+        this.make('product').data$(msg.data).save$(respond);
+    });
+
+    seneca.add({ role: 'product', cmd: 'get' }, function (msg, respond) {
+        this.make('product').load$(msg.data.user_id, respond);
+    });
+
+    seneca.add({ role: 'product', cmd: 'get-all' }, function (msg, respond) {
+        this.make('product').list$({}, respond);
+    });
+
+    seneca.add({ role: 'product', cmd: 'delete-all' }, function (msg, respond) {
+        this.make('product').remove$(msg.data.user_id, respond);
+    });
+
+}
+
+module.exports = plugin;
